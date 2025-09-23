d13240d6deff0720027661a7946e3f6010bd416b
Gorge <gorge213@Gorges-MacBook-Pro.local>
2025-09-23 20:11:14 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/IntellijenciaArtificialApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijenciaArtificialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijenciaArtificialApplication.class, args);
+	}
+
+}
